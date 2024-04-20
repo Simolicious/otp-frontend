9023cf177f2da1f157646660b598761b35396966
Smon <simonthomas2001@gmail.com>
2024-04-20 13:57:21 +0000

    Added necessary elements and styles to the pages as well as routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OtpSubmitAndResendComponent } from './otp-submit-and-resend/otp-submit-and-resend.component';
+import { OtpConfirmComponent } from './otp-confirm/otp-confirm.component';
 
 const routes: Routes = [
   {
     path: '',
     component: OtpSubmitAndResendComponent,
+  },
+  {
+    path: 'confirm',
+    component: OtpConfirmComponent,
   }
 ];
 
--- src/app/otp-submit-and-resend/otp-submit-and-resend.component.ts
+++ src/app/otp-submit-and-resend/otp-submit-and-resend.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-otp-submit-and-resend',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtpSubmitAndResendComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.router.navigate(['confirm']);
+  }
 }
